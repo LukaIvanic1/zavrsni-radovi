f6036b91acf6fa1e0a579540f6939c90bbd34c59
Luka Ivanić <107309593+LukaIvanic1@users.noreply.github.com>
2024-04-24 09:34:37 +0200

    Update petehv2.ino

--- SP/petehv2.ino
+++ SP/petehv2.ino
@@ -1,6 +1,3 @@
-// Receiving IR codes: https://github.com/Arduino-IRremote/Arduino-IRremote?tab=readme-ov-file#receiving-ir-codes
-// Compile options / macros for this library: https://github.com/Arduino-IRremote/Arduino-IRremote?tab=readme-ov-file#compile-options--macros-for-this-library
-
 #include <Arduino.h>
 #include <Servo.h>
 
@@ -10,8 +7,10 @@ Servo servo;
 #define RAW_BUFFER_LENGTH 128
 #define MARK_EXCESS_MICROS 20
 #define RECORD_GAP_MICROS 5000
-//#define DEBUG
+
 int offset = 15;
+
+int pogon = 4;
 
 #include <IRremote.hpp>
 
@@ -20,27 +19,41 @@ void setup() {
     IrReceiver.begin(IR_RECEIVE_PIN, false);
     servo.attach(3); 
     servo.write(90);
+    pinMode(pogon,OUTPUT);
 }
 
 void loop() {
   if (!IrReceiver.decode()) return;
+
   IrReceiver.resume();
-  //IrReceiver.printIRResultShort(&Serial);
 
   if (IrReceiver.decodedIRData.protocol != 8) return;
   if (IrReceiver.decodedIRData.address != 0) return;
 
   Serial.println(IrReceiver.decodedIRData.command);
 
-  if(IrReceiver.decodedIRData.command == 70){
-    //motor dela
+
+  int cmd = IrReceiver.decodedIRData.command; 
+  if(cmd == 70){
+    pogonNaprijed();   
   }
 
-  if(IrReceiver.decodedIRData.command == 68){
+  if(cmd == 68){
     servo.write(120+offset);
+    pogonNaprijed();
+    servo.write(90);
   }
-  if(IrReceiver.decodedIRData.command == 67){
+  if(cmd == 67){
     servo.write(60-offset);
+    pogonNaprijed();
+    servo.write(90);
   }
 
 }
+
+void pogonNaprijed(){
+  digitalWrite(pogon,HIGH);
+  delay(2000);
+  digitalWrite(pogon,LOW);
+}
+
